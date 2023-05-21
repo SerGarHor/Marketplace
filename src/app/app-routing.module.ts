7eebd103f2a5465da10285feff085785c2cb88a2
sagarzon18 <sagarzon18@ucatolica.edu.co>
2023-05-21 14:15:54 -0500

    ajustes del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ShoppingcartComponent } from './shoppingcart/shoppingcart.component';
-import { environment } from '../environments/environment.prod';
 
-let routes: Routes = [];
-if (environment.production) {
-  routes = [
-    { path: '', redirectTo: `${environment.appPath}home`, pathMatch: 'full' },
-    { path: `${environment.appPath}home`, component: HomeComponent },
-    { path: `${environment.appPath}home/shoppingcart`, component: ShoppingcartComponent },
-  ];
-} else {
+let routes: Routes = []
   routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'home/shoppingcart', component: ShoppingcartComponent },
   ];
-}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
